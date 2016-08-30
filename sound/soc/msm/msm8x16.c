e025b703439fb5c03143717842df40ae033497c0
Meng Wang <mwang@codeaurora.org>
2016-08-30 10:35:49 +0800

    Fixing 192kHz for all sample rates.
    ASoC: utils: initialize dummy_codec before use
    
    dummy_codec is not initialized before use, which
    could cause kernel panic. Initialize dummy_codec before use.
    
    Change-Id: Iedf7a3accbd14138ab7ed9e4e36a98fd7ca9a839
    Signed-off-by: Meng Wang <mwang@codeaurora.org>

--- sound/soc/msm/msm8x16.c
+++ sound/soc/msm/msm8x16.c
@@ -34,10 +34,6 @@
 #include "../codecs/wcd9306.h"
 #define DRV_NAME "msm8x16-asoc-wcd"
 
-#define SAMPLING_RATE_48KHZ 48000
-#define SAMPLING_RATE_96KHZ 96000
-#define SAMPLING_RATE_192KHZ 192000
-
 #define BTSCO_RATE_8KHZ 8000
 #define BTSCO_RATE_16KHZ 16000
 #define MAX_SND_CARDS 3
@@ -233,13 +229,6 @@ static struct cdc_pdm_pinctrl_info pinctrl_info;
 struct ext_cdc_tlmm_pinctrl_info ext_cdc_pinctrl_info;
 
 static int mi2s_rx_bit_format = SNDRV_PCM_FORMAT_S16_LE;
-static int mi2s_rx_sample_rate = SAMPLING_RATE_48KHZ;
-static int bits_per_sample = 16;
-
-struct msm8909_auxcodec_prefix_map {
-	char codec_name[50];
-	char codec_prefix[25];
-};
 
 static inline int param_is_mask(int p)
 {
@@ -281,8 +270,6 @@ static const struct snd_soc_dapm_widget msm8x16_dapm_widgets[] = {
 
 static char const *rx_bit_format_text[] = {"S16_LE", "S24_LE"};
 static const char *const ter_mi2s_tx_ch_text[] = {"One", "Two"};
-static const char *const mi2s_tx_ch_text[] = {"One", "Two", "Three", "Four"};
-static char const *mi2s_rx_sample_rate_text[] = {"KHZ_48", "KHZ_96", "KHZ_192"};
 static const char *const loopback_mclk_text[] = {"DISABLE", "ENABLE"};
 static const char *const lineout_text[] = {"DISABLE", "ENABLE", "DUALMODE"};
 
@@ -339,7 +326,7 @@ static int msm_pri_rx_be_hw_params_fixup(struct snd_soc_pcm_runtime *rtd,
 
 	pr_debug("%s: Number of channels = %d\n", __func__,
 			msm_pri_mi2s_rx_ch);
-	rate->min = rate->max = 48000;
+	rate->min = rate->max = 192000;
 	channels->min = channels->max = msm_pri_mi2s_rx_ch;
 
 	return 0;
@@ -355,59 +342,8 @@ static int msm_be_hw_params_fixup(struct snd_soc_pcm_runtime *rtd,
 					SNDRV_PCM_HW_PARAM_CHANNELS);
 
 	pr_debug("%s()\n", __func__);
-	rate->min = rate->max = mi2s_rx_sample_rate;
+	rate->min = rate->max =192000;
 	channels->min = channels->max = 2;
-
-	return 0;
-}
-
-static int mi2s_rx_sample_rate_get(struct snd_kcontrol *kcontrol,
-	struct snd_ctl_elem_value *ucontrol)
-{
-	int sample_rate_val = 0;
-
-	switch (mi2s_rx_sample_rate) {
-	case SAMPLING_RATE_192KHZ:
-		sample_rate_val = 2;
-		break;
-
-	case SAMPLING_RATE_96KHZ:
-		sample_rate_val = 1;
-		break;
-
-	case SAMPLING_RATE_48KHZ:
-	default:
-		sample_rate_val = 0;
-		break;
-	}
-
-	ucontrol->value.integer.value[0] = sample_rate_val;
-	pr_debug("%s: mi2s_rx_sample_rate = %d\n", __func__,
-				mi2s_rx_sample_rate);
-
-	return 0;
-}
-
-static int mi2s_rx_sample_rate_put(struct snd_kcontrol *kcontrol,
-	struct snd_ctl_elem_value *ucontrol)
-{
-	pr_debug("%s: ucontrol value = %ld\n", __func__,
-			ucontrol->value.integer.value[0]);
-
-	switch (ucontrol->value.integer.value[0]) {
-	case 2:
-		mi2s_rx_sample_rate = SAMPLING_RATE_192KHZ;
-		break;
-	case 1:
-		mi2s_rx_sample_rate = SAMPLING_RATE_96KHZ;
-		break;
-	case 0:
-	default:
-		mi2s_rx_sample_rate = SAMPLING_RATE_48KHZ;
-	}
-
-	pr_debug("%s: mi2s_rx_sample_rate = %d\n", __func__,
-			mi2s_rx_sample_rate);
 
 	return 0;
 }
@@ -645,7 +581,7 @@ static int msm_bta2dp_be_hw_params_fixup(struct snd_soc_pcm_runtime *rtd,
 	struct snd_interval *channels = hw_param_interval(params,
 					SNDRV_PCM_HW_PARAM_CHANNELS);
 
-	rate->min = rate->max = 48000;
+	rate->min = rate->max = 192000;
 	channels->min = channels->max = 2;
 
 	return 0;
@@ -665,7 +601,7 @@ static int msm_proxy_rx_be_hw_params_fixup(struct snd_soc_pcm_runtime *rtd,
 	if (channels->max < 2)
 		channels->min = channels->max = 2;
 	channels->min = channels->max = msm_proxy_rx_ch;
-	rate->min = rate->max = 48000;
+	rate->min = rate->max = 192000;
 	return 0;
 }
 
@@ -675,7 +611,7 @@ static int msm_proxy_tx_be_hw_params_fixup(struct snd_soc_pcm_runtime *rtd,
 	struct snd_interval *rate = hw_param_interval(params,
 					SNDRV_PCM_HW_PARAM_RATE);
 
-	rate->min = rate->max = 48000;
+	rate->min = rate->max = 192000;
 	return 0;
 }
 
@@ -688,7 +624,7 @@ static int msm_tx_be_hw_params_fixup(struct snd_soc_pcm_runtime *rtd,
 					SNDRV_PCM_HW_PARAM_CHANNELS);
 
 	pr_debug("%s(), channel:%d\n", __func__, msm_ter_mi2s_tx_ch);
-	rate->min = rate->max = 48000;
+	rate->min = rate->max = 192000;
 	channels->min = channels->max = msm_ter_mi2s_tx_ch;
 
 	return 0;
@@ -1026,9 +962,6 @@ static const struct soc_enum msm_snd_enum[] = {
 	SOC_ENUM_SINGLE_EXT(2, ter_mi2s_tx_ch_text),
 	SOC_ENUM_SINGLE_EXT(2, loopback_mclk_text),
 	SOC_ENUM_SINGLE_EXT(3, lineout_text),
-	SOC_ENUM_SINGLE_EXT(6, pri_rx_sample_rate_text),
-	SOC_ENUM_SINGLE_EXT(6, mi2s_tx_sample_rate_text),
-	SOC_ENUM_SINGLE_EXT(2, mi2s_rx_sample_rate_text),
 };
 
 static const char *const btsco_rate_text[] = {"8000", "16000"};
@@ -1050,12 +983,6 @@ static const struct snd_kcontrol_new msm_snd_controls[] = {
 	SOC_ENUM_EXT("Lineout_1 amp", msm_snd_enum[3],
 			lineout_status_get, lineout_status_put),
 
-	SOC_ENUM_EXT("RX SampleRate", msm_snd_enum[3],
-			pri_rx_sample_rate_get, pri_rx_sample_rate_put),
-	SOC_ENUM_EXT("MI2S TX SampleRate", msm_snd_enum[4],
-			mi2s_tx_sample_rate_get, mi2s_tx_sample_rate_put),
-	SOC_ENUM_EXT("MI2S_RX SampleRate", msm_snd_enum[3],
-			mi2s_rx_sample_rate_get, mi2s_rx_sample_rate_put),
 };
 
 static int msm8x16_mclk_event(struct snd_soc_dapm_widget *w,
@@ -3034,3 +2961,4 @@ MODULE_DESCRIPTION("ALSA SoC msm");
 MODULE_LICENSE("GPL v2");
 MODULE_ALIAS("platform:" DRV_NAME);
 MODULE_DEVICE_TABLE(of, msm8x16_asoc_machine_of_match);
+
