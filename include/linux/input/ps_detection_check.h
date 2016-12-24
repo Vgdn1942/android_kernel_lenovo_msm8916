cf79cee399ac58714db738e95de6a2ab18e9d676
Vgdn1942 <vgdn1942@gmail.com>
2016-12-24 22:01:47 +0300

    Add ps_detection_check

---
+++ include/linux/input/ps_detection_check.h
@@ -0,0 +1,6 @@
+#ifndef _LINUX_TOUCH_WAKE_FIX_H
+#define _LINUX_TOUCH_WAKE_FIX_H
+
+int epl259x_pocket_detection_check(void); // If proximity sensor no close return 1; else return 0
+
+#endif // _LINUX_TOUCH_WAKE_FIX_H
