33036cec4b415b65e96fb76900ecc88b60840fc4
vgdn1942 <vgdn1942@gmail.com>
2017-02-21 12:25:09 +0300

    ReAdd wake_gesture & pocket_mod

--- include/linux/input/ps_detection_check.h
+++
@@ -1,8 +0,0 @@
-#ifndef _LINUX_PS_DETECTION_CHECK_H
-#define _LINUX_PS_DETECTION_CHECK_H
-
-int epl259x_pocket_detection_check(void); // If proximity sensor return value 0-65536
-
-int device_is_pocketed(void); // Return value 1 for close or 0 for open
-
-#endif // _LINUX_PS_DETECTION_CHECK_H
